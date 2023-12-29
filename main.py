1d881193dcd0b4fba73ea3346c766b5a80e7cb38
v1asavi <124231657+v1asavi@users.noreply.github.com>
2023-12-29 10:01:14 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+ import streamlit as st
+st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
+#ing colour of test
+st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
+st.file_uploader("upload file")
